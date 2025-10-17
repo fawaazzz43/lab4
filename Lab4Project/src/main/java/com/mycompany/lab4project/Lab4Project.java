6c014dd4363eee3730f2951e2c3dc7bc84bc05bf
MohamedRabie74 <es.Mohamed.rabea2023@alexu.edu.eg>
2025-10-17 14:47:39 +0300

    Added first version of Lab4 project

---
+++ Lab4Project/src/main/java/com/mycompany/lab4project/Lab4Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab4project;
+
+/**
+ *
+ * @author pc
+ */
+public class Lab4Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub!");
+    }
+}
